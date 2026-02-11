60ad1ca830f37eed9528dd83f220751c87a31b46
dfn-slxxp <waldman.sebastian@gmail.com>
2026-02-11 12:34:24 -0500

    Complete Lesson 9

---
+++ Annual Intro to CS/Semester 2/Lesson 9/combined_cw_hw_challenge.py
@@ -0,0 +1,24 @@
+# Classwork
+
+def upperLower():
+    s = input("Enter a string: ")
+    print(s.upper(), s.lower(), sep="\n")
+
+
+
+# Homework
+
+def capFirstLast(s):
+    return s[0].upper() + s[1:-1] + s[-1].upper()
+
+def swaCase(s):
+    return s.swapcase()
+
+
+# Homework
+
+def upperIfUpper(s):
+    count = 0
+    for i in range(4):
+        count += 1 if s[i].isupper() else 0
+    return s.upper() if count >= 2 else s
