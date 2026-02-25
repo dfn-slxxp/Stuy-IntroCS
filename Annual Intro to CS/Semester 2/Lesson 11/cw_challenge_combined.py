18f8802bb1f3cdd6812884e6cde5819c513eb604
dfn-slxxp <waldman.sebastian@gmail.com>
2026-02-24 20:38:43 -0500

    Complete challenge 11 (this was a fun one)

--- Annual Intro to CS/Semester 2/Lesson 11/cw_challenge_combined.py
+++ Annual Intro to CS/Semester 2/Lesson 11/cw_challenge_combined.py
@@ -27,3 +27,36 @@ def myUpper(str):
 
 # Challenge
 
+def alphabeticalSentance(str):
+    # Split the sentance into words that are all lowercase
+    words = str.lower().split()
+
+    print(*words)
+
+
+    # Initialize empty list for sorted words
+    sorted_words = []
+
+    # Sort the list
+    while len(words) > 0:
+        # Initialize the first index
+        first = 0
+
+        # Check to see if a word exists that comes before the "first" word alphabetically
+        for i in range(len(words)):
+            if words[first] > words[i]:
+                # If such word exists, set "first" to that word
+                first = i
+
+        # Remove the word with the index of "first" and add it to sorted list
+        sorted_words.append(words.pop(first))
+
+    # Create an empty string to store final result
+    final_str = ""
+
+    # Iterate through items in sorted list and add each to the final string
+    for word in sorted_words:
+        final_str = final_str + word + " "
+
+    # Return the result
+    return final_str
