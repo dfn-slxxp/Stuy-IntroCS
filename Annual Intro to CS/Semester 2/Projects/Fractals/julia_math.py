faacef944b181dc81a84ea0d8f71fa2941ca538f
dfn-slxxp <waldman.sebastian@gmail.com>
2026-04-16 21:28:38 -0400

    warren do smth

---
+++ Annual Intro to CS/Semester 2/Projects/Fractals/julia_math.py
@@ -0,0 +1,8 @@
+def generate_julia_set():
+    return
+
+# make it return an arrlist of tuples for the rgb value
+# (0-255, 0-255, 0-255)
+# make the rows and columns variable and an input
+# any constant that can be changed should be in the input
+# and we will have user input
